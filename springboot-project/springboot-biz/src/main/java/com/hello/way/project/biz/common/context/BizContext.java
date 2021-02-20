52e4e2dc432912213494f93785cd46e502816811
tongchuanwei <tongchuanwei10448@hellobike.com>
2021-02-20 16:25:11 +0800

    上下文对象

---
+++ springboot-project/springboot-biz/src/main/java/com/hello/way/project/biz/common/context/BizContext.java
@@ -0,0 +1,43 @@
+package com.hello.way.project.biz.common.context;
+
+/**
+ * 通用业务上下文
+ * @author way
+ * @since 2020-01-07
+ */
+public class BizContext extends Context {
+
+    private static final String CONTEXT_KEY_BOS_USER_INFO         =   "bos_user";
+    private static final String CONTEXT_KEY_ADMIN_USER_INFO       =   "admin_user";
+
+    /**
+     * 设置BOS用户信息
+     */
+    public static void setBosUserInfo() {
+        current().put(CONTEXT_KEY_BOS_USER_INFO, null);
+    }
+
+    /**
+     * 获取BOS用户信息
+     * @return
+     */
+    public static Object getBosUserInfo() {
+        return current().get(CONTEXT_KEY_BOS_USER_INFO);
+    }
+
+    /**
+     * 设置Admin用户信息
+     */
+    public static void setAdminUserInfo() {
+        current().put(CONTEXT_KEY_ADMIN_USER_INFO, null);
+    }
+
+    /**
+     * 获取Admin用户信息
+     * @return
+     */
+    public static Object getAdminUserInfo() {
+        return current().get(CONTEXT_KEY_ADMIN_USER_INFO);
+    }
+
+}
