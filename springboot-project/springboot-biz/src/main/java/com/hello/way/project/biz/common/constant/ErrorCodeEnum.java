e0551e172fe54a83297eaef6ec047a60e4bfa2ed
tongchuanwei <tongchuanwei10448@hellobike.com>
2021-02-20 16:47:12 +0800

    RpcInterceptor

--- springboot-project/springboot-biz/src/main/java/com/hello/way/project/biz/common/constant/ErrorCodeEnum.java
+++ springboot-project/springboot-biz/src/main/java/com/hello/way/project/biz/common/constant/ErrorCodeEnum.java
@@ -43,8 +43,8 @@ public enum ErrorCodeEnum {
     FAILED(500, "操作失败"),
     VALIDATE_FAILED(404, "参数检验失败"),
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    FORBIDDEN(403, "没有相关权限");
-
+    FORBIDDEN(403, "没有相关权限"),
+    UN_KNOW_ERROR(404, "未知异常");
 
 
     private Integer code;
